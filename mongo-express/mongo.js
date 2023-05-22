309e3c893b7ff8631326bf61234df7aa6293acac
Hionel <104563991+Hionel@users.noreply.github.com>
2023-05-22 15:12:24 +0300

    dotenv i and created another mongo project

--- mongo-express/mongo.js
+++ mongo-express/mongo.js
@@ -1,6 +1,8 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-	"mongodb+srv://hionel:3iOtMMhv2AoijzFE@ngcommerce.yft8zvu.mongodb.net/?retryWrites=true&w=majority";
+const dotenv = require("dotenv");
+
+dotenv.config();
+const uri = `mongodb+srv://hionel:${process.env.MONGO_PASS}@mongotrials.ocypofw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
